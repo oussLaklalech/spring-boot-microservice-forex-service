72a86a3ad6b480ad909b9ed9615a85a825a49b43
Laklalech Oussama <olaklalech@SQLI756-8.SQLI>
2018-05-03 10:13:42 +0200

    create project and initialize first call REST

---
+++ src/main/java/com/olc/springboot/microservice/example/forex/springbootmicroserviceforexservice/SpringBootMicroserviceForexServiceApplication.java
@@ -0,0 +1,12 @@
+package com.olc.springboot.microservice.example.forex.springbootmicroserviceforexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
+	}
+}
